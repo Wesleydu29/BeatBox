3c888d3560ede2cef719c5d23fce0ee3b852e26f
Wesley Goarant <Wesleydu29860@gmail.com>
2025-10-23 15:47:41 +0200

    init tracks and create step buttons

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.relativelayout import RelativeLayout
+from kivy.properties import ObjectProperty
+from track import TrackWidget
 
+Builder.load_file("track.kv")
+
+NB_TRACKS = 4
 class MainWidget(RelativeLayout):
-    pass
+    tracks_layout = ObjectProperty()
+
+    def on_parent(self, widget, parent):
+        for i in range(0, NB_TRACKS):
+            self.tracks_layout.add_widget(TrackWidget())
+
 
 class BeatBoxApp(App):
     pass
