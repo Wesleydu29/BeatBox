47a9e8e72f28ffc241842dad37b92fcccb038cc6
Wesley Goarant <Wesleydu29860@gmail.com>
2025-10-23 15:06:52 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from kivy.app import App
+from kivy.uix.relativelayout import RelativeLayout
+
+class MainWidget(RelativeLayout):
+    pass
+
+class BeatBoxApp(App):
+    pass
+
+BeatBoxApp().run()
